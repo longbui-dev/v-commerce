a04446c243788f6d61f982c17bb858b517eff9d2
thao vy <lttvy2@cmcglobal.vn>
2022-12-30 18:08:54 +0700

    [hotfix/11-fix-layout-slide-and-advertisement-products] fix layout

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import Footer from './components/footer/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container relative">
       <Header />
       <Slide />
       <AdvertisementProducts />
--- src/components/navbar/Header.tsx
+++ src/components/navbar/Header.tsx
@@ -2,12 +2,13 @@ import Logo from "./logo";
 import NavBar from "./menu";
 import Toolbar from "./toolbar";
 import { Col, Row } from "antd";
+import './Header.scss';
 
 function Header() {
   return (
-    <Row className="flex justify-around p-6 absolute z-10 w-full">
+    <Row className="flex justify-around absolute z-10 w-full header">
       <Col span={6} className="flex justify-center"><Logo /></Col>
-      <Col span={12} className="flex justify-center"> <NavBar /></Col>
+      <Col span={12} className="flex justify-center"><NavBar /></Col>
       <Col span={6} className="flex justify-center"><Toolbar /></Col>
     </Row>
   );
