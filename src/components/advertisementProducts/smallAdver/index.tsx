1de3b794940813a4a0b25df048679528ed81b569
thao vy <lttvy2@cmcglobal.vn>
2023-01-05 15:15:47 +0700

    [10-advertiment-products] fix responsive Advertiment

--- src/components/advertisementProducts/smallAdver/index.tsx
+++ src/components/advertisementProducts/smallAdver/index.tsx
@@ -6,10 +6,10 @@ const { Meta } = Card;
 const SmallAdver = (props: any) => (
   <Card
     hoverable
-    cover={<img alt="advertimentProducts" src={props.image} className='px-14 py-6'/>}
+    cover={<img alt="advertimentProducts" src={props.image} className='px-14 py-6 smallAdverImg'/>}
     className='w-60 bg-slate-100 productHover'
   >
-    <Meta className='capitalize text-center' title={props.title} description={props.description} />
+    <Meta className='capitalize text-center smallAdverInfor' title={props.title} description={props.description} />
   </Card>
 );
 
