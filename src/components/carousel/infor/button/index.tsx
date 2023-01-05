7f326195be9d6157719329a6f03ab719243aaf84
thao vy <lttvy2@cmcglobal.vn>
2023-01-05 13:46:58 +0700

    [9-implement-the-slider] fix responsive slide

--- src/components/carousel/infor/button/index.tsx
+++ src/components/carousel/infor/button/index.tsx
@@ -4,15 +4,15 @@ import './Button.scss'
 
 function ButtonComponent(props: any) { 
     return (
-        <Space className='flex flex-row items-start my-12'>
-            <Button type="primary" className='flex justify-center capitalize p-8 text-base font-medium buttonAdd'>
+        <Space className='flex flex-row items-start my-8 styleMobile'>
+            <Button type="primary" className='flex justify-center capitalize p-8 text-base font-medium buttonAdd buttonStyles'>
                 <div className='flex self-center'>
                     <PlusCircleOutlined className='flex justify-center hover:text-orange-500 mt-1 mr-2'/>
                     add to cart
                 </div>
             </Button>
 
-            <Button type="primary" className='flex justify-center capitalize p-8 text-base font-medium bg-slate-100 border-slate-100 hover:border-orange-500 buttonMore'>
+            <Button type="primary" className='flex justify-center capitalize p-8 text-base font-medium bg-slate-100 border-slate-100 buttonStyles hover:border-orange-500 buttonMore'>
                 <div className='flex self-center'>
                     more info
                 </div>
--- src/components/carousel/infor/price/index.tsx
+++ src/components/carousel/infor/price/index.tsx
@@ -2,7 +2,7 @@ import './Price.scss'
 
 function Price(props: any) { 
     return (
-        <div className="flex flex-row justify-start items-start pt-6">
+        <div className="flex flex-row justify-start items-start pt-4">
             <div className="flex flex-row justify-start items-start font-medium price">$ 399.000</div><del className="text-stone-400 pl-4 font-medium">$ 499.000</del>
         </div>
     )
