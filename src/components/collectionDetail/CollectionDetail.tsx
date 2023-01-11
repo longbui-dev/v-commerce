9792873ce0f1ad870999b35785bd08245b1cd637
thao vy <lttvy2@cmcglobal.vn>
2023-01-11 09:13:48 +0700

    [15-mock-fake-data] fake data collection

--- src/components/collectionDetail/CollectionDetail.tsx
+++ src/components/collectionDetail/CollectionDetail.tsx
@@ -1,4 +1,5 @@
 import { Button, Col, Row, Space } from 'antd';
+import { collection } from '../../mockdata/Collection';
 import './CollectionDetail.scss';
 
 function CollectionDetail() {
@@ -8,9 +9,9 @@ function CollectionDetail() {
         <div className="container collection">
           <Row className='px-32 inforCollection'>
             <Col span={12} className="block justify-start gutter-row inforCollectionCol">
-              <h2 className='capitalize'>unlimited dainning table collection</h2>
-              <p className='font-bold'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-              <div className='collectionPrice font-bold mb-8 text-sm'><h4 className='capitalize text-white'>string from <span className='mainColor'>$ 299</span></h4></div>
+              <h2 className='capitalize'>{collection.data[0].productName}</h2>
+              <p className='font-bold'>{collection.data[0].description}</p>
+              <div className='collectionPrice font-bold mb-8 text-sm'><h4 className='capitalize text-white'>string from <span className='mainColor'>$ {collection.data[0].price}</span></h4></div>
               <Space className='h-12 leading-10 mainColorBg rounded-lg p-6 cursor-pointer'>
                 <Button type="primary" className='capitalize shadow-none'>
                   <div className='flex self-center font-bold text-base'>view more</div>
