4ac2eb3b833ad559c653137f61b792e7e9bd1b9e
thao vy <lttvy2@cmcglobal.vn>
2023-01-12 13:48:07 +0700

    [18-feature-news-letter]

---
+++ src/components/newsLetter/information/index.tsx
@@ -0,0 +1,20 @@
+
+
+function InformationNewsLetter(props: any) {
+    return (
+      <div className='p-5'>
+        <div>
+          <h4 className='flex justify-start text-lg font-bold mb-11 capitalize text-zinc-600'>information</h4>
+          <div className="">
+            <p className='flex justify-start text-sm text-zinc-600 capitalize textHover'>about us</p>
+            <p className='flex justify-start text-sm text-zinc-600 capitalize textHover'>contact us</p>
+            <p className='flex justify-start text-sm text-zinc-600 capitalize textHover'>news</p>
+            <p className='flex justify-start text-sm text-zinc-600 capitalize textHover'>store</p>
+          </div>
+        </div>
+       
+      </div>
+    );
+}            
+
+export default InformationNewsLetter;
