cc4efa77372a86108e129217a2f175307f9f8cb0
ThaoVy <thaovyluuthanh@gmail.com>
2023-01-14 14:42:05 +0700

    [21-feature-click-icon-cart-on-header-show-list-products]

---
+++ src/mockdata/ProductsCart.ts
@@ -0,0 +1,27 @@
+import imgSlider1 from "../assets/img/slider1";
+import imgSlider2 from "../assets/img/slider2";
+import imgSlider3 from "../assets/img/slider3";
+
+export const productsCart = {
+  message: "success",
+  data: [
+    {
+      id: 1,
+      productName: "arm chair",
+      price: 399.02,
+      image: imgSlider1,
+    },
+    {
+      id: 2,
+      productName: "single arm chair",
+      price: 499.02,
+      image: imgSlider2,
+    },
+    {
+      id: 3,
+      productName: "wooden arm chair",
+      price: 599.02,
+      image: imgSlider3,
+    },
+  ],
+};
