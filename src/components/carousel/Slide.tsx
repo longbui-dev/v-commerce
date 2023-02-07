ce8075068db0d9a8b581bbdd63ae0420ed56b2c0
thao vy <lttvy2@cmcglobal.vn>
2023-02-07 14:05:36 +0700

    [25-get-api-in-page-carousel]

--- src/components/carousel/Slide.tsx
+++ src/components/carousel/Slide.tsx
@@ -1,29 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Col, Row, Carousel } from 'antd';
 import Picture from './picture';
 import InforCarousel from './infor';
-import { slideDatas } from '../../mockdata/Slide';
 import './Slide.scss';
+import { advertisementProducts } from '../../store/slices/allProducts';
+import { useSelector } from 'react-redux';
 
-const Slide: React.FC = (props) => (
-  <div id='slide' className='bg-slate-100 -m-px'>
-    <Carousel autoplay  dots={{ className: "dots"}} className="flex justify-center"> 
-      {
-        slideDatas.data.map((slideData) => (
-          <div key={slideData.id}>
-            <div className="headerStyle">
-              <Row className="flex justify-center pb-10 z-10 w-full container slideDisplayMobile" gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-                <Col span={14} className="flex justify-center gutter-row colInfor">
-                  <InforCarousel productName={slideData.productName} description={slideData.description} price={slideData.price} oldPrice={slideData.oldPrice}/>
-                </Col>
-                <Col span={10} className="flex justify-center gutter-row colPicture"> <Picture image={slideData.image}/></Col>
-              </Row>
+function Slide() {
+  const carouselProductShow = useSelector(advertisementProducts)
+
+  return (
+    <div id='slide' className='bg-slate-100 -m-px'>
+      <Carousel autoplay dots={{ className: "dots"}} className="flex justify-center"> 
+        {
+          carouselProductShow.map((productShow : any) => (
+            <div key={productShow.idAdvertisementProducts}>
+              <div className="headerStyle">
+                <Row className="flex justify-center pb-10 z-10 w-full container slideDisplayMobile" gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+                  <Col span={14} className="flex justify-center gutter-row colInfor">
+                    <InforCarousel productName={productShow.titleAdvertisementProducts} description={productShow.describeAdvertisementProducts} price={productShow.priceAdvertisementBigProducts} oldPrice={productShow.priceAdvertisementBigProducts}/>
+                  </Col>
+                  <Col span={10} className="flex justify-center gutter-row colPicture"> <Picture image={productShow.imageAdvertisementProducts}/></Col>
+                </Row>
+              </div>
             </div>
-          </div>
-        ))
-      }
-    </Carousel>
-  </div>
-);
+          ))
+        }
+      </Carousel>
+    </div>
+  )
+}
     
 export default Slide;
