05c3b0563c280e6b3bc1c01d7aebf3d24abcd6c3
thao vy <lttvy2@cmcglobal.vn>
2023-03-02 15:51:14 +0700

    page cart

--- src/components/pageCart/index.tsx
+++ src/components/pageCart/index.tsx
@@ -1,84 +1,89 @@
-import React, { useState } from "react";
-import { DeleteOutlined } from "@ant-design/icons";
-import { Button, Popconfirm, Space, Table } from "antd";
-import type { ColumnsType } from "antd/es/table";
-import "./index.scss";
-import { useSelector } from "react-redux";
+import React, { useState } from 'react'
+import { DeleteOutlined } from '@ant-design/icons'
+import { Button, Popconfirm, Space, Table } from 'antd'
+import type { ColumnsType } from 'antd/es/table'
+import './index.scss'
+import { useSelector } from 'react-redux'
 import {
   selectProductsInCart,
   selectTotalPrice,
-} from "../../store/slices/amountProductsInCart";
+} from '../../store/slices/amountProductsInCart'
 
 interface CartType {
-  Key: React.Key;
-  title: string;
-  price: number;
-  amount: number;
+  Key: React.Key
+  title: string
+  price: number
+  amount: number
+  // payPerCol: number
 }
 
 function PageCart() {
-  const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+  const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([])
+  // const [payPerCol, setPayPerCol] = useState(0)
 
   const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
-    setSelectedRowKeys(newSelectedRowKeys);
-  };
+    setSelectedRowKeys(newSelectedRowKeys)
+  }
   const rowSelection = {
     selectedRowKeys,
     onChange: onSelectChange,
-  };
-  const hasSelected = selectedRowKeys.length > 0;
+  }
+  const hasSelected = selectedRowKeys.length > 0
 
-  const productsInCart = useSelector(selectProductsInCart);
-  let totalPrice = useSelector(selectTotalPrice);
+  const productsInCart = useSelector(selectProductsInCart)
 
-  const uniqueIds: CartType[] = [];
+  const uniqueIds: CartType[] = []
   productsInCart.forEach((e: any) => {
     if (uniqueIds.indexOf(e.id) === -1) {
-      uniqueIds.push(e.id);
+      uniqueIds.push(e.id)
     }
-  });
+  })
 
   const cart = uniqueIds.map((uq) => {
-    const products = productsInCart.filter((e: any) => e.id === uq);
+    const products = productsInCart.filter((e: any) => e.id === uq)
     return {
       ...products[0],
       amount: products.length,
       key: products[0].id,
-    };
-  });
+      // payPerCol: products[0].price * products[0].amount,
+    }
+  })
+  // console.log(productsInCart, 'selectedRowKeys')
 
   const columnsCart: ColumnsType<CartType> = [
     {
-      title: "Product",
-      dataIndex: "title",
-      key: "productName",
-      sortDirections: ["descend", "ascend"],
+      title: 'Product',
+      dataIndex: 'title',
+      key: 'productName',
+      sortDirections: ['descend', 'ascend'],
       sorter: (a, b) => a.title.localeCompare(b.title),
     },
     {
-      title: "Price",
-      dataIndex: "price",
-      key: "price",
-      width: "12%",
+      title: 'Price',
+      dataIndex: 'price',
+      key: 'price',
+      width: '12%',
       sorter: (a, b) => a.price - b.price,
     },
     {
-      title: "Amount",
-      dataIndex: "amount",
-      width: "10%",
-      key: "amount",
+      title: 'Amount',
+      dataIndex: 'amount',
+      width: '10%',
+      key: 'amount',
       sorter: (a, b) => a.amount - b.amount,
     },
     {
-      title: "Pay",
-      width: "20%",
-      key: "totalPay",
-      render: (row: any) => row.price * row.amount,
-      // sorter: (a, b) => a.price * b.amount,
+      title: 'Pay',
+      width: '20%',
+      key: 'payPerCol',
+      render: (row: any) => {
+        // console.log(row.amount, ' row.price * row.amount')
+        return row.price * row.amount
+      },
     },
     {
-      title: "Action",
-      dataIndex: "action",
+      title: 'Action',
+      dataIndex: 'action',
       render: (_, row) => (
         <Space size="middle" className="cursor-pointer">
           <Popconfirm title="Sure to delete?" className="popconfirm">
@@ -86,15 +91,15 @@ function PageCart() {
           </Popconfirm>
         </Space>
       ),
-      width: "10%",
+      width: '10%',
     },
-  ];
+  ]
 
   return (
     <div id="pageCart">
       <div className="block m-auto container py-8 pageCart">
         <div className="py-6">
-          {hasSelected ? `Selected ${selectedRowKeys.length} products` : ""}
+          {hasSelected ? `Selected ${selectedRowKeys.length} products` : ''}
         </div>
         <Table
           rowSelection={rowSelection}
@@ -108,7 +113,7 @@ function PageCart() {
                   Total Pay
                 </Table.Summary.Cell>
                 <Table.Summary.Cell colSpan={2} index={1}>
-                  {hasSelected ? totalPrice : "0"}
+                  {hasSelected ? selectedRowKeys : '0'}
                 </Table.Summary.Cell>
               </Table.Summary.Row>
             </Table.Summary>
@@ -131,7 +136,7 @@ function PageCart() {
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default PageCart;
+export default PageCart
